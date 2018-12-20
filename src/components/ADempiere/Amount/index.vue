35f41899c1a7c1af9967f395ab14b634cbd37811
Leonel Matos <matosleonel0@gmail.com>
2018-12-20 17:10:02 -0400

    Amont Component
    this component has been modified today

--- src/components/ADempiere/Amount/index.vue
+++ src/components/ADempiere/Amount/index.vue
@@ -1,34 +1,49 @@
-
-<!-- the :precision attribute indicating the decimal quantity -->
+<!-- the :precision attribute indicating the decimal quantity
+:min="data.ValueMin"
+      :max="data.ValueMax"-->
 <template>
-  <el-input-number v-model="val" :precision="data.Decimals" :min="data.ValueMin" :max="data.ValueMax" controls-position="right" @change="handleChange" @keyup.native="validatePattern" />
+  <div>
+    <el-input-number
+      v-model="val"
+      :precision="data.Decimals"
+      :step="data.Steps"
+      :disabled="data.IsReadOnly"
+      value="0"
+      controls-position="right"
+      @change="handleChange"
+      @keyup.native="validatePattern" />
+  </div>
 </template>
 
 <script>
+import dataJson from '@/views/ADempiere/data/field/dataAttributes.Amount.json'
 export default {
-  name: 'ADAmount',
+  name: 'Amount',
   props: {
     data: {
       type: Object,
       default: () => ({
-        'ValueMin': 0,
-        'ValueMax': 10,
-        'Decimals': 2
+        ...dataJson.data
       })
     }
   },
   data() {
     return {
-      val: 1
+      val: 0
     }
   },
   methods: {
     handleChange(value) {
       const _Pattern = /[^0-9.]/g
-      this.val = value.target.value.replace(_Pattern, '')
+      const res = _Pattern.test(this.val)
+      if (res === true) {
+        this.val.replace(_Pattern, '')
+      }
+      /* this.val = value.target.value.replace(_Pattern, 'A')
+      console.log(value)*/
     },
     validatePattern(value) {
-      console.log(value.target.value)
+      // console.log(value.target.value)
       value = value.target.value.toString()
       const _Pattern = /[^0-9.]/g
       this.val = value.replace(_Pattern, '')
