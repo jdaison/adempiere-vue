9f86edb724db76910456e36f26d07303dcc4c80b
elsio <elsiosanches@gmail.com>
2018-12-20 17:13:11 -0400

    date component
    finished date component

---
+++ src/views/ADempiere/DateDemo/index.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <reference :data-attributes="data"/>
+</template>
+
+<script>
+import dataJSON from '@/views/ADempiere/data/field/dataAttributes.Date.json'
+import Reference from '@/components/ADempiere/Reference'
+
+export default {
+  name: 'DateDemo',
+  components: {
+    Reference
+  },
+  data() {
+    return {
+      data: {
+        ...dataJSON
+      }
+    }
+  }
+}
+</script>
