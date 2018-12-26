cb197c95da6112abf10d7a32b3fd2767c2ce2342
Leonel Matos <matosleonel0@gmail.com>
2018-12-26 16:50:40 -0400

    Suport to  integer component
    Migration of the integer component

---
+++ src/views/ADempiere/IntegerDemo/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-container">
+    <references :data-attributes="data"/>
+  </div>
+</template>
+<script>
+import dataJson from '@/views/ADempiere/data/field/dataAttributes.Integer.json'
+import References from '@/components/ADempiere/Reference.vue'
+export default {
+  name: 'IntegerDemo',
+  components: {
+    References
+  },
+  data() {
+    return {
+      data: {
+        ...dataJson
+      }
+    }
+  }
+}
+</script>
+<style></style>
