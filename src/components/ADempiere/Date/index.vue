b57b8dda1416d8348d5b7b4ee9d8312bddb68c93
Unknown <elsiosanches@gmail.com>
2018-12-26 16:18:12 -0400

    Add validation type date

--- src/components/ADempiere/Date/index.vue
+++ src/components/ADempiere/Date/index.vue
@@ -1,31 +1,59 @@
 
 <template>
-  <div class="block">
-    <span class="demonstration">seleccione fecha</span>
+  <div >
+    <span class="demonstration">Default</span>
     <el-date-picker
-      v-model="data1"
+      v-model="value1"
+      :format="data.VFormat"
       type="date"
       placeholder="Pick a day"
     />
-
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Date',
+  name: 'ADDate',
+  props: {
+    data: {
+      type: Object,
+      default: () => ({})
+    }
+  },
   data() {
     return {
       pickerOptions1: {
         disabledDate(time) {
-          return time.getTime() < Date.now()
-        }
+          return time.getTime() > Date.now()
+        },
+        shortcuts: [{
+          text: 'Today',
+          onClick(picker) {
+            picker.$emit('pick', new Date())
+          }
+        }, {
+          text: 'Yesterday',
+          onClick(picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24)
+            picker.$emit('pick', date)
+          }
+        }, {
+          text: 'A week ago',
+          onClick(picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', date)
+          }
+        }]
       },
-      data1: ''
+      value1: '',
+      value2: ''
     }
   }
 }
 </script>
 
-<style></style>
+<style>
+
+</style>
