0edd6b42d5ae1b7b4362f319e8ddfc1867577549
Leonel Matos <matosleonel0@gmail.com>
2018-12-27 14:30:06 -0400

    Binary component (upload file)
    support and migration of binary component (upload file)

---
+++ src/views/ADempiere/BinaryDemo/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app-container">
+    <field :data-attributes="data"/>
+  </div>
+</template>
+<script>
+import dataJSON from '@/views/ADempiere/data/field/dataAttributes.Binary.json'
+import Field from '@/components/ADempiere/Field'
+export default {
+  name: 'Binary',
+  components: {
+    Field
+  },
+  data() {
+    return {
+      data: {
+        ...dataJSON
+      }
+    }
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      this.$message.success(`The previously uploaded file has been deleted.`)
+    },
+    handleSuccess(file) {
+      this.$message.success(`The file has been successfully loaded.`)
+    },
+    handleError(file) {
+      this.$message.error(`The file does not meet the specifications.`)
+    }
+  }
+}
+</script>
+<style></style>
