ae19d91e410a05116c39e5678f257085154210f4
EdwinBetanc0urt <EdwinBetanc0urt@hotmail.com>
2018-12-27 22:38:57 -0400

    minimal changes

--- src/components/ADempiere/YesNo/index.vue
+++ src/components/ADempiere/YesNo/index.vue
@@ -4,15 +4,14 @@
     v-model="value"
     active-text="Yes"
     inactive-text="Not"
-    active-value="y"
-    inactive-value="n"
+    active-value="Y"
+    inactive-value="N"
     @change="handleChange(value)"/>
-
 </template>
 
 <script>
 export default {
-  name: 'Yes',
+  name: 'YesNo',
   props: {
     data: {
       type: Object,
