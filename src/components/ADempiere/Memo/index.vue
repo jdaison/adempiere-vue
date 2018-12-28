7a399da47e10abe9d9a34c5fe5b064179f625c7a
Unknown <elsiosanches@gmail.com>
2018-12-28 16:59:50 -0400

    /#14-Memo: Auto stash before revert of "Support to Name Printer Component"

---
+++ src/components/ADempiere/Memo/index.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div>
+    <el-input
+      v-model="input"
+      :autosize="{ minRows: 2, maxRows: 4}"
+      type="textarea"
+      placeholder="A description is limited to 255 characters."/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Memo',
+  props: {
+
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      input: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
