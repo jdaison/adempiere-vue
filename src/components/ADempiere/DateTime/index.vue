6a45b240b54d3fbf604d826cb3255579b040a6f0
Unknown <elsiosanches@gmail.com>
2019-01-08 08:57:45 -0400

    in process

--- src/components/ADempiere/DateTime/index.vue
+++ src/components/ADempiere/DateTime/index.vue
@@ -1,66 +1,38 @@
 <template>
   <div class="block">
-    <span class="demonstration">Default</span>
     <el-date-picker
-      v-model="value1"
-      type="datetime"
-      placeholder="Select date and time"
-      format="dd/MM/yyyy HH:mm:ss"/>
+      v-model="setData.val"
+      :picker-options="{
+      selectableRange: range}"
 
-    <span class="demonstration">With shortcuts</span>
-    <el-date-picker
-      v-model="value2"
-      :picker-options="pickerOptions1"
+      :format="data.V"
       type="datetime"
       placeholder="Select date and time"
-      format="dd/MM/yyyy HH:mm:ss"/>
+    />
 
-    <span class="demonstration">With default time</span>
-    <el-date-picker
-      v-model="value3"
-      type="datetime"
-      placeholder="Select date and time"
-      default-time="12:00:00"
-      format="dd/MM/yyyy HH:mm:ss"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ADDateTime',
+  name: 'DateTime',
   props: {
     data: {
       type: Object,
-      default: () => ({})
+      required: true
     }
+  },
+  setData: {
+    type: Object,
+    default: () => ({
+      val: new Date(),
+      min: '2019-01-01 ',
+      max: '2019-01-10'
+    })
   },
   data() {
     return {
-      pickerOptions1: {
-        shortcuts: [{
-          text: 'Today',
-          onClick(picker) {
-            picker.$emit('pick', new Date())
-          }
-        }, {
-          text: 'Yesterday',
-          onClick(picker) {
-            const date = new Date()
-            date.setTime(date.getTime() - 3600 * 1000 * 24)
-            picker.$emit('pick', date)
-          }
-        }, {
-          text: 'A week ago',
-          onClick(picker) {
-            const date = new Date()
-            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
-            picker.$emit('pick', date)
-          }
-        }]
-      },
-      value1: '',
-      value2: '',
-      value3: ''
+      range: this.setData.min + '-' + this.setData.max
     }
   }
 }
---
+++ src/views/ADempiere/TimeDemo/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-container">
+    <field :data-attributes="data" />
+  </div>
+</template>
+
+<script>
+import dataJSON from '@/views/ADempiere/data/field/dataAttributes.Time.json'
+import Field from '@/components/ADempiere/Field'
+export default {
+  name: 'TimeDemo',
+  components: {
+    Field
+  },
+  data() {
+    return {
+      data: {
+        ...dataJSON
+      }
+    }
+  }
+}
+</script>
+<style></style>
