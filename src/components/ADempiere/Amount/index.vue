dcc1cab9493dd5e7505755429b09b8db9108202d
Leonel Matos <matosleonel0@gmail.com>
2019-01-15 15:45:23 -0400

    reuse component test

--- src/components/ADempiere/Amount/index.vue
+++ src/components/ADempiere/Amount/index.vue
@@ -1,19 +1,25 @@
 <template>
-  <el-input-number
-    v-model="val"
-    :precision="data.Decimals"
-    :step="data.Steps"
-    :disabled="data.IsReadOnly"
-    :value="data.DefaultValue"
-    :min="data.ValueMin"
-    :max="data.ValueMax"
-    controls-position="right"
-  />
+  <div>
+    <el-input-number
+      v-model="val"
+      :precision="data.Decimals"
+      :step="data.Steps"
+      :disabled="data.IsReadOnly"
+      :value="data.DefaultValue"
+      :min="data.ValueMin"
+      :max="data.ValueMax"
+      controls-position="right"/>
+    <Integer/>
+  </div>
 </template>
 
 <script>
+import Integer from '@/views/ADempiere/IntegerDemo'
 export default {
   name: 'Amount',
+  components: {
+    Integer
+  },
   props: {
     data: {
       type: Object,
