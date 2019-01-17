aa7d1c0553d646533681c67c783134fb1cd4cd94
Leonel Matos <matosleonel0@gmail.com>
2019-01-17 10:24:18 -0400

    Support location component
    support this component

---
+++ src/views/ADempiere/LocationDemo/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-form class="app-container">
+    <field :data-attributes="data"/>
+  </el-form>
+</template>
+
+<script>
+import dataJSON from '@/views/ADempiere/data/field/dataAttributes.Location.json'
+import Field from '@/components/ADempiere/Field'
+
+export default {
+  name: 'LocationDemo',
+  components: {
+    Field
+  },
+  data() {
+    return {
+      data: {
+        ...dataJSON
+      }
+    }
+  }
+}
+</script>
